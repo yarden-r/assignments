0a7dcc7dac1952536fb8b0bbb61606b96c820469
yarden.regev <yardisome@gmail.com>
2022-08-05 20:58:00 +0300

    made tiny tweek to report

--- guardicore_crawler/components/report.py
+++ guardicore_crawler/components/report.py
@@ -12,6 +12,7 @@ class Report(Singleton):
 
 # public methods
     def __init__(self,filename):
+        self.file_name = filename
         f = FileController(filename,'a')
         self.logger = Logger(f)
         self.tracker_map = {}
@@ -25,23 +26,25 @@ class Report(Singleton):
         if not issubclass(type(item),ReportItem):
             raise TypeError('item must be of type ReportItem')
             
-        if not self.__is_visited(item.name):
+        if not self.is_visited(item):
             self.__add_item_to_map(item.name)
             self.__write(item)
             self.item_counter += 1
+            return True
+        return False
 
     def get_num_of_items(self):
         return self.item_counter
 
+    def is_visited(self,item):
+        return self.tracker_map.get(item.name) is not None
+    
 # private methods
     def __write(self, item):
         self.logger.log(str(item))
     
     def __add_item_to_map(self,item):
         self.tracker_map[item] = True
-    
-    def __is_visited(self,item):
-        return self.tracker_map.get(item) is not None
     
 
 
